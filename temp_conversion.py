ce898f963b773737c8180bd9ccd90e33053bea49
Talesa Howell <cpafi@Talesas-MacBook-Pro-2.local>
2022-09-13 08:39:33 -0400

    completion of challenge 2

--- temp_conversion.py
+++ temp_conversion.py
@@ -5,26 +5,37 @@ def convert_100_to_celsius():
     # Is the resulting temperature you get an integer or float?
     # Print 'float' if it is a float or 'int' if it is an int
     # How do you know? Write a comment below your code explaining how you know which it is
-
+    celsius_100 = (100-32)*5/9
+    print(celsius_100)
+    print("float")
+convert_100_to_celsius()
 
 def convert_0_to_celsius():
     # Convert a temperature of 0 degrees fahrenheit to celsius
     # Save this to a variable called celsius_0, and use print() to print out the value
-
+    celsius_0 = (0-32)*5/9
+    print(celsius_0)
+convert_0_to_celsius()
 
 def convert_34_2_to_celsius():
     # Convert a temperature of 34.2 degrees fahrenheit to celsius
     # Do this one all in one print statement without saving any variables
-
+    print((34.2-32)*5/9)
+convert_34_2_to_celsius()
 '''
 Now, can you convert back?
 '''
 
 def convert_5_to_fahrenheit():
     # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
-
+    fahrenheit_5 = (5 + 32)*9/5
+    print(fahrenheit_5)
+convert_5_to_fahrenheit()
+    
 
 def hotter_temp():
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
-
+    hotter_temp = (30.2 * 9/5)+ 32
+    print(hotter_temp)
+hotter_temp()
